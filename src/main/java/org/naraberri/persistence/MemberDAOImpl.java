99b0d1c9938c7dad9c64c8037fe5bd05fe5ddd47
LEE SEUNG CHAN <LEE SEUNG CHAN@DESKTOP-SBKPE9B>
2017-01-02 11:28:18 +0900

    update

--- src/main/java/org/naraberri/persistence/MemberDAOImpl.java
+++ src/main/java/org/naraberri/persistence/MemberDAOImpl.java
@@ -1,5 +1,8 @@
 package org.naraberri.persistence;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.inject.Inject;
 
 import org.apache.ibatis.session.SqlSession;
@@ -11,18 +14,33 @@ public class MemberDAOImpl implements MemberDAO {
 
 	@Inject
 	private SqlSession sqlSession;
-	
-	private static final String namespace =
-			"org.naraberri.mapper.MemberMapper";
-	
+
+	private static final String namespace = "org.naraberri.mapper.MemberMapper";
+
 	@Override
 	public String getTime() {
-		return sqlSession.selectOne(namespace+".getTime");
+		return sqlSession.selectOne(namespace + ".getTime");
 	}
 
 	@Override
 	public void insertMamber(MemberVO vo) {
-		sqlSession.insert(namespace+".insertMember",vo);		
+		sqlSession.insert(namespace + ".insertMember", vo);
+	}
+
+	@Override
+	public MemberVO readMember(String userid) throws Exception {
+		return (MemberVO) sqlSession.selectOne(namespace + ".selectMember", userid);
+	}
+
+	@Override
+	public MemberVO readWithPW(String userid, String userpw) throws Exception {
+
+		Map<String, Object> paramMap = new HashMap<String, Object>();
+
+		paramMap.put("userid", userid);
+		paramMap.put("userpw", userpw);
+
+		return sqlSession.selectOne(namespace + ".readWithPW",  paramMap);
 	}
 
 }
